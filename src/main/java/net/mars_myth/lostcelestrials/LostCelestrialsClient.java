cbdacb2b4e977424103d675a2f1506013078002b
MarsMyth <callumshelton2503@gmail.com>
2024-09-21 09:33:34 +1000

    Added Client.java and changed fabric.mod.json

---
+++ src/main/java/net/mars_myth/lostcelestrials/LostCelestrialsClient.java
@@ -0,0 +1,10 @@
+package net.mars_myth.lostcelestrials;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LostCelestrialsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
